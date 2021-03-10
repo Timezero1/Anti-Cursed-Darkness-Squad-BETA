689f8523a071efb62d7fcf206597fc488bf09619
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-03-10 15:53:47 +0100

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,12 @@
+import os
+
+
+os.system('sudo apt update')
+os.system('curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash')
+os.system('sudo apt install npm')
+os.system('nvm install node')
+os.system('npm install')
+os.system('sudo apt-get install python3-pip')
+os.system('pip3 install colorama')
+os.system('pip3 install requests')
+os.system('python3 darkness.py')
